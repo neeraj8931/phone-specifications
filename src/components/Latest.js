8f53fb9d79339b126be108efb8f348608e2bc450
Neeraj <neeraj.shukla@shoptimize.in>
2022-05-25 17:23:04 +0530

    Latest Phone Issue Fixed

--- src/components/Latest.js
+++ src/components/Latest.js
@@ -3,35 +3,39 @@ import { useState,useEffect } from 'react';
 
 export default function Latest() {
     const [latest, setLatest] = useState([]);
+    const [latestPhones, setLatestPhones] = useState([]);
     const getData = () => {
         fetch(`https://api-mobilespecs.azharimm.site/v2/latest`)
           .then((res) => res.json())
           .then((res) => {
-            console.log(res.data);
             setLatest(res.data);
+            setLatestPhones(res.data.phones);
+            console.log(latestPhones);
           })
       }
 
     useEffect(() => {
         getData();
-    }, [])
-    let showLatestModals= latest.phones.map(phone=>{
-        return <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a href="#" className="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" className="w-3/5 mx-auto  object-center w-full h-full" src={phone.image}/> 
-        </a>
+        console.log(latestPhones)
+        latestPhones.map(i=>console.log("ok"));
+    },[ ])
+    
+    let showLatestPhones= latestPhones.map(phone=>{
+        return <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class=" w-4/5 h-auto block" src={phone.image} />
+        </div>
         <div className="mt-4">
-          <h2 className=" text-center text-gray-900 title-font text-lg font-medium">{phone.phone_name}</h2>
+          <h2 className="text-gray-900 title-font text-lg font-medium">{phone.phone_name}</h2>
         </div>
       </div>
     })
-    
   return (
     <section class="text-gray-600 body-font">
         <h2 className='text-center text-5xl'>{latest.title}</h2>
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap -m-4">
-        {showLatestModals}
+        {showLatestPhones}
     </div>
   </div>
 </section>
